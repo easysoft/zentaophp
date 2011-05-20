0dea2c85ebf043b193122ddfef735da8277b132e
wwccss <chunsheng@cnezsoft.com>
2011-05-20 07:39:08 +0000

    + import the my.js.

---
+++ app/demo/www/js/my.js
@@ -0,0 +1,52 @@
+/**
+ * The js file of ZenTaoPHP.
+ *
+ * The author disclaims copyright to this source code.  In place of
+ * a legal notice, here is a blessing:
+ * 
+ *  May you do good and not evil.
+ *  May you find forgiveness for yourself and forgive others.
+ *  May you share freely, never taking more than you give.
+ */
+
+/**
+ * Create link. 
+ * 
+ * @param  string $moduleName 
+ * @param  string $methodName 
+ * @param  string $vars 
+ * @param  string $viewType 
+ * @access public
+ * @return string
+ */
+function createLink(moduleName, methodName, vars, viewType)
+{
+    if(!viewType) viewType = config.defaultView;
+    if(vars)
+    {
+        vars = vars.split('&');
+        for(i = 0; i < vars.length; i ++) vars[i] = vars[i].split('=');
+    }
+    if(config.requestType == 'PATH_INFO')
+    {
+        link = config.webRoot + moduleName + config.requestFix + methodName;
+        if(vars)
+        {
+            if(config.pathType == "full")
+            {
+                for(i = 0; i < vars.length; i ++) link += config.requestFix + vars[i][0] + config.requestFix + vars[i][1];
+            }
+            else
+            {
+                for(i = 0; i < vars.length; i ++) link += config.requestFix + vars[i][1];
+            }
+        }
+        link += '.' + viewType;
+    }
+    else
+    {
+        link = config.router + '?' + config.moduleVar + '=' + moduleName + '&' + config.methodVar + '=' + methodName + '&' + config.viewVar + '=' + viewType;
+        if(vars) for(i = 0; i < vars.length; i ++) link += '&' + vars[i][0] + '=' + vars[i][1];
+    }
+    return link;
+}
