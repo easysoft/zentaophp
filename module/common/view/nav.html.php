29c9d0cca074dc924cd78e54f896361850d48277
wwccss <chunsheng@cnezsoft.com>
2013-05-14 14:10:27 +0800

    * adjust the lang switcher.

--- module/common/view/nav.html.php
+++ module/common/view/nav.html.php
@@ -19,12 +19,12 @@
           }
           ?>
         </ul>
-        <div class="btn-group pull-right">
+        <div class='btn-group pull-right' id='nav-right'>
           <?php 
           foreach($config->langs as $langCode => $langLabel)
           {
               $btnClass = $app->getClientLang() == $langCode ? 'btn-primary' : '';
-              echo html::a($app->getURI(), $langLabel, '', "class='btn btn-small $btnClass lang-switcher' data-lang='$langCode'");
+              echo "<button class='btn btn-small $btnClass lang-switcher mt-10px' data-lang='$langCode' onclick='switchLang(this)'>$langLabel</button>";
           }
           ?>
         </div>
