4021a86f238f428178f5bbe0b39d79c8c10c13a1
Osub <814566123@qq.com>
2015-06-28 10:15:59 +0800

    修改注释错误
    修改注释错误

--- framework/control.class.php
+++ framework/control.class.php
@@ -110,7 +110,7 @@ class control
     public $cookie;
 
     /**
-     * $global对象，将$_COOKIE数组改为对象，方便调用。
+     * $global对象，将$_GLOBAL数组改为对象，方便调用。
      * The $global object, used to access the $_GLOBAL var.
      * 
      * @var ojbect
