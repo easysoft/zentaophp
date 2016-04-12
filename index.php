0e987c87675afdaac1963658cc880a6350b90e7b
wwccss <chunsheng@cnezsoft.com>
2016-04-12 13:06:16 +0800

    * adjust the comments for index.php.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * 整个应用的入口文件。
  * The router file of zentaophp.
  *
  * All request should be routed by this router.
@@ -11,26 +12,21 @@
  *  May you find forgiveness for yourself and forgive others.
  *  May you share freely, never taking more than you give.
  */
-/* Set the error reporting. */
+/* 先关闭所有的错误输出。Turn off error reporting first. */
 error_reporting(0);
 
-/* Start output buffer. */
+/* 使用ob扑获所有的输出。Use ob to get output. */
 ob_start();
 
-/* Load the framework. */
+/* 加载框架核心文件。Load the framework. */
 include './framework/router.class.php';
 include './framework/control.class.php';
 include './framework/model.class.php';
 include './framework/helper.class.php';
 
-/* Instance the app. */
-$startTime = getTime();
-$app = router::createApp('demo', dirname(__FILE__), 'router');
-
-/* Run the app. */
-$common = $app->loadCommon();
-$app->parseRequest();
-$app->loadModule();
-
-/* Flush the buffer. */
-echo helper::removeUTF8Bom(ob_get_clean());
+/* 响应请求。Response the request. */
+$app    = router::createApp('demo', dirname(__FILE__), 'router');     // 实例化router。  Instance the router class.
+$common = $app->loadCommon();                                         // 加载common模块。Load the common module.
+$app->parseRequest();                                                 // 解析请求。      Parse the request.
+$app->loadModule();                                                   // 加载模块。      Load module.
+echo helper::removeUTF8Bom(ob_get_clean());                           // 输出内容。      Print the output.
