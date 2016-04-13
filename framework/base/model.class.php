c4d0dc389de71da9c27c2fb09892a901a3b4e29f
wwccss <chunsheng@cnezsoft.com>
2016-04-13 12:57:01 +0800

    * adjust comments.

--- framework/base/model.class.php
+++ framework/base/model.class.php
@@ -3,19 +3,14 @@
  * ZenTaoPHP的baseModel类。
  * The baseModel class file of ZenTaoPHP framework.
  *
+ * @package framework
+ *
  * The author disclaims copyright to this source code.  In place of
  * a legal notice, here is a blessing:
  * 
  *  May you do good and not evil.
  *  May you find forgiveness for yourself and forgive others.
  *  May you share freely, never taking more than you give.
- */
-
-/**
- * baseModel基类。
- * The base class of model.
- * 
- * @package framework
  */
 class baseModel
 {
