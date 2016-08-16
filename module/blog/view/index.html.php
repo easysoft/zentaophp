3e49fe7158eb532b3454ecf76c5a122d2f4cfd2e
guanxiying <guanxiying@xirnagit.com>
2016-08-16 16:48:28 +0800

    * Adjust style of block list.

--- module/blog/view/index.html.php
+++ module/blog/view/index.html.php
@@ -12,19 +12,24 @@
 ?>
 <?php include '../../common/view/header.html.php';?>
 <div class='container'>
-  <table class='table table-bordered table-hover'> 
+  <div class='panel'>
+  <div class='panel-heading'>
+    <strong> <?php echo $lang->blog->index;?></strong>
+    <div class='pull-right'> <?php echo html::a(inlink('create'), $lang->blog->add, "class='btn btn-primary btn-xs'");?></div>
+  </div>
+  <table class='table table-list table-hover'> 
     <thead>
       <tr>
-        <th width='50'><?php echo $lang->blog->id;?></th>  
-        <th><?php echo $lang->blog->title;?></th>  
-        <th width='150'><?php echo $lang->blog->date;?></th>  
-        <th width='120'><?php echo $lang->blog->action;?></th>  
+        <td width='50'><?php echo $lang->blog->id;?></td>  
+        <td><?php echo $lang->blog->title;?></td>  
+        <td class='text-center' width='150'><?php echo $lang->blog->date;?></td>  
+        <td class='text-center' width='120'><?php echo $lang->blog->action;?></td>  
       </tr>
     </thead>
     <tbody>
       <?php foreach($articles as $article):?>
       <tr>
-        <td><?php echo $article->id;?></td>
+        <td class='text-center'><?php echo $article->id;?></td>
         <td><?php echo $article->title;?></td>
         <td><?php echo $article->date;?></td>
         <td>
@@ -41,12 +46,12 @@
       <tr>
         <td colspan='4'>
           <?php 
-          echo html::a(inlink('create'), $lang->blog->add, "class='btn btn-primary btn-sm'");
           $pager->show('right', 'short');
           ?>
         </td>
       </tr>  
     </tfoot>
   </table>
+</div>
 </div>
 <?php include '../../common/view/footer.html.php';?>
