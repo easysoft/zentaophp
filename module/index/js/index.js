6111135ce6468d9d712b2aa9f22936e8647e9f6e
guanxiying <guanxiying@xirnagit.com>
2016-08-18 13:59:45 +0800

    * Add css and js file.

---
+++ module/index/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function()
+{
+if(0){
+
+    $('.col-md-3').mouseover(function(){
+      $(this).find('h4').hide();
+      $(this).find('p').show();
+    });
+    $('.col-md-3').mouseout(function(){
+      $(this).find('h4').show();
+      $(this).find('p').hide();
+    });
+}
+})
